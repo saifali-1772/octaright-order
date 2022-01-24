1a94914c190092b78850dc099faa0af3cc7f35a7
saif ali <saifalishaikh.email@gmail.com>
2022-01-24 16:43:55 +0530

    octaright order

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const nodemailer = require("nodemailer");
+const port = process.env.PORT || 3000
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/thanks', (req, res) => {
+    res.render('thanks')
+})
+
+app.post('/submit',async (req, res) => {
+    // res.send("name:"+req.body.name)
+    let transporter = await nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'octaright@gmail.com',
+            pass: 'octaright@123'
+        }
+    });
+
+    let mailOptions = await {
+        from: '"Octaright"octaright@gmail.com',
+        to: 'saifalishaikh.dev@gmail.com',
+        subject: 'New Website Order',
+        text: `Hi Octaright
+
+   I am intrasted this offer, This my details
+   
+   Name : ${req.body.name}
+   Email : ${req.body.email}
+   Phone : ${req.body.phone}
+   
+   Plz call me sir.`
+        // html: text_var
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            return res.json(error);
+        } else {
+            return res.json('Email sent: ' + info.response);
+            // return res.json({ expires: expires })
+        }
+    });
+
+    res.redirect('/thanks')
+})
+
+app.get('*', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port)
